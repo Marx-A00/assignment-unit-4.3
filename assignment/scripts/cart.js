375a24778e10070b38a63b0b5c6b9b764d052f45
Marcos Andrade <mnandrade1999@gmail.com>
2023-10-10 17:25:16 -0500

    completed first few functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,6 +1,40 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item){
+    basket.push(item);
+
+    for(let i of basket){
+        if(i == item){
+            return true;
+        }
+    }
+    return false;
+}
+console.log("Should return true: ", addItem("fedora"));
+
+function listItems(){
+    if(basket.length == 0){
+        return ("Basket is empty");
+    }
+    for(let i of basket){
+        console.log(i+"\n");
+    }
+}
+addItem("gold chain");
+addItem("Master Chief helmet");
+addItem("1 Vans skate shoe");
+listItems();
+
+function empty(){
+    basket.length = 0;
+    return ("Basket has been emptied");
+}
+empty();
+console.log(listItems());
+
 
 
 
